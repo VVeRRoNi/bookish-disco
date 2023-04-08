e4367f117103a30c52764371cf82f6e26516e6ac
VVeRRoNi <122989600+VVeRRoNi@users.noreply.github.com>
2023-04-08 17:39:12 +0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+TOKEN = '5771573897:AAGP8OtLWeNBVO9d565f4TlX-4C_THnJQXU'
+keys = {
+    'BTC': 'BTC',
+    'ETH': 'ETH',
+    'USD': 'USD',
+    'EUR': 'EUR',
+    'RUB': 'RUB',
+}
+
+keys2 = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+}
